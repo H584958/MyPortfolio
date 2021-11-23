a76bed70f47d73cbe3281c7472a7e56fdce06fe3
H584958 <584958@stud.hvl.no>
2021-11-23 22:34:02 +0100

    Added fadeIn scroll to about me

--- src/PortfolioContainer/AboutMe/AboutMe.js
+++ src/PortfolioContainer/AboutMe/AboutMe.js
@@ -5,9 +5,18 @@ import Animations from "../../utilities/Animations";
 import "./AboutMe.css";
 
 export default function AboutMe(props) {
-    return (
-        <div>
+    let fadeInScreenHandeler = (screen) => {
+        if(screen.fadeInScreen !== props.id)
+        return
+        Animations.animations.fadeInScreen(props.id)
+    }
 
-        </div>
-    )
+    const fadeInSubscription = ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandeler)
+
+  return (
+    <div className="about-me-container screen-container" id={props.id || ""}>
+      <div className="about-me-parent"></div>
+      <ScreenHeading title={'About Me'} subHeading={'Why Choose Me?'}/>
+    </div>
+  );
 }
