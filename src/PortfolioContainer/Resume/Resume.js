6d03b8dd5f01bcbd62a2f9cfff5e649919e4ee1a
H584958 <584958@stud.hvl.no>
2021-11-24 17:37:35 +0100

    finalized resume

--- src/PortfolioContainer/Resume/Resume.js
+++ src/PortfolioContainer/Resume/Resume.js
@@ -8,6 +8,13 @@ export default function Resume(props) {
   const [selectedBulletIndex, setSelectedBulletIndex] = useState(0);
   const [carousalOffsetStyle, setCarousalOffsetStyle] = useState({});
 
+  let fadeInScreenHandler = (screen) => {
+    if (screen.fadeInScreen !== props.id) return;
+    Animations.animations.fadeInScreen(props.id);
+  };
+  const fadeInSubscription =
+    ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
+
   const ResumeHeading = (props) => {
     return (
       <div className="resume-heading">
@@ -83,39 +90,130 @@ export default function Resume(props) {
       />
     </div>,
     <div className="resume-screen-container" key="work-experience">
-      <div className="experience-container">
+      <ResumeHeading
+        heading={"HVL"}
+        subHeading={"TEACHERASSISSTANT"}
+        fromDate={"2020"}
+        toDate={"Present"}
+      />
+      <div className="experience-description">
+        <span className="resume-description-text">
+          Currently working as TA.
+        </span>
+      </div>
+      <div className="experience-description">
+        <span className="resume-description-text">
+          - Assist the lecturer with the correction of assignments and help
+          students find good solutions to assignments.
+        </span>
+      </div>
+      ,
+      <div
+        className="resume-screen-container programming-skills-container"
+        key="programming-skills"
+      >
+        {programmingSkillsDetails.map((skill, index) => (
+          <div className="skill-parent" key={index}>
+            <div className="heading-bullet"></div>
+            <span>{skill.skill}</span>
+            <div className="skill-percentage">
+              <div
+                style={{ width: skill.ratingPercentage + "%" }}
+                className="active-percentage-bar"
+              ></div>
+            </div>
+          </div>
+        ))}
+      </div>
+      ,
+      <div className="resume-screen-container" key="projects">
+        {projectsDetails.map((projectsDetails, index) => (
+          <ResumeHeading
+            key={index}
+            heading={projectsDetails.title}
+            subHeading={projectsDetails.subHeading}
+            description={projectsDetails.description}
+            fromDate={projectsDetails.duration.fromDate}
+            toDate={projectsDetails.duration.toDate}
+          />
+        ))}
+      </div>
+      ,
+      <div className="resume-screen-container" key="interests">
         <ResumeHeading
-          heading={"HVL"}
-          subHeading={"TEACHERASSISSTANT"}
-          fromDate={"2020"}
-          toDate={"Present"}
+          heading="Teaching"
+          description="Apart from being a tech enthusiast and a code writer, i also love to teach people what i know simply because i believe in sharing."
         />
-        <div className="experience-description">
-          <span className="resume-description-text">
-            Currently working as TA.
-          </span>
-        </div>
-        <div className="experience-description">
-          <span className="resume-description-text">
-            - Assist the lecturer with the correction of assignments and help
-            students find good solutions to assignments.
-          </span>
-        </div>
+        <ResumeHeading
+          heading="Chess"
+          description="Looking at a smooth chess game is something i can never compromise with, and I can never get enough of the feeling i get when i win a game."
+        />
+        <ResumeHeading
+          heading="Hiking"
+          description="I like the relaxing feeling that comes with going for a hike in the mountains, not to mention the great sights you get to see."
+        />
       </div>
     </div>,
   ];
 
-  let fadeInScreenHandler = (screen) => {
-    if (screen.fadeInScreen !== props.id) return;
-    Animations.animations.fadeInScreen(props.id);
+  const handleCarousal = (index) => {
+    let offsetHeight = 360;
+
+    let newCarousalOffset = {
+      style: { transform: "translateY(" + index * offsetHeight * -1 + "px)" },
+    };
+
+    setCarousalOffsetStyle(newCarousalOffset);
+    setSelectedBulletIndex(index);
   };
-  const fadeInSubscription =
-    ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
+
+  const getBullets = () => {
+    return resumeBullets.map((bullet, index) => (
+      <div
+        onClick={() => handleCarousal(index)}
+        className={
+          index === selectedBulletIndex ? "bullet selected-bullet" : "bullet"
+        }
+        key={index}
+      >
+        <img
+          className="bullet-logo"
+          src={require(`../../assets/Resume/${bullet.logoSrc}`).default}
+          alt="Error"
+        />
+        <span className="bullet-label">{bullet.label}</span>
+      </div>
+    ));
+  };
+
+  const getResumeScreens = () => {
+    return (
+      <div
+        style={carousalOffsetStyle.style}
+        className="resume-details-carousal"
+      >
+        {resumeDetails.map((ResumeDetail) => ResumeDetail)}
+      </div>
+    );
+  };
 
   return (
-    <div className="resume-container screen-container" id={props.id || ""}>
+    <div
+      className="resume-container screen-container fade-in"
+      id={props.id || ""}
+    >
       <div className="resume-content">
-        <ScreenHeading title={"Resume"} subHeading={"My Formal Bio Details"} />
+        <ScreenHeading title={"Resume"} subHeading={"My formal Bio Details"} />
+        <div className="resume-card">
+          <div className="resume-bullets">
+            <div className="bullet-container">
+              <div className="bullet-icons"></div>
+              <div className="bullets">{getBullets()}</div>
+            </div>
+          </div>
+
+          <div className="resume-bullet-details">{getResumeScreens()}</div>
+        </div>
       </div>
     </div>
   );
