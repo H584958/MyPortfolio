92081eeccb5fb2465e1c719df4e2b43e1a409881
H584958 <584958@stud.hvl.no>
2021-11-25 12:45:03 +0100

    ContactMe.js start

--- src/PortfolioContainer/ContactMe/ContactMe.js
+++ src/PortfolioContainer/ContactMe/ContactMe.js
@@ -2,27 +2,50 @@ import React, { useState } from "react";
 import ScreenHeading from "../../utilities/ScreenHeading/ScreenHeading";
 import ScrollService from "../../utilities/ScrollService";
 import Animations from "../../utilities/Animations";
-import './ContactMe.css' 
+import "./ContactMe.css";
 
 export default function ContactMe(props) {
+  let fadeInScreenHandler = (screen) => {
+    if (screen.fadeInScreen !== props.id) return;
 
-    let fadeInScreenHandler = (screen) => {
-        if (screen.fadeInScreen !== props.id) return;
-    
-        Animations.animations.fadeInScreen(props.id);
-      };
-    const fadeInSubscription = ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
-    
-    return (
-        <div className="contact-me-container screen-container" id={props.id || ""}>
-          <div className="contact-me-content">
-            <ScreenHeading title={"Contact Me"} subHeading={"Ask me anything"} />
-            <div className="contact-card">
-              
+    Animations.animations.fadeInScreen(props.id);
+  };
+
+  let render = () => {
+    return <input type="text" value="" />;
+  };
+
+  const fadeInSubscription =
+    ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
+
+  return (
+    <div className="contact-me-container screen-container" id={props.id || ""}>
+      <div className="contact-me-content">
+        <ScreenHeading title={"Contact Me"} subHeading={"Ask me anything"} />
+        <div className="contact-me-card">
+          <div className="contact-me-heading">
+            <span>Get In Touch📨</span>
+            <div className="contact-me-info"></div>
+            <div className="contact-me-box">
+              <div className="name-text-box">
+                <span>Name:</span>
+                <br />
+                {render()}
+              </div>
+              <div className="email-text-box">
+                <span>Email:</span>
+                <br />
+                {render()}
+              </div>
+              <div className="message-text-box">
+                <span>Message:</span>
+                <br />
+                {render()}
+              </div>
             </div>
           </div>
         </div>
-      );
-    
-    
+      </div>
+    </div>
+  );
 }
