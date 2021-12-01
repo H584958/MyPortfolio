f5e97fa8b344c91571a15be2edd2312a0ff02cb7
H584958 <584958@stud.hvl.no>
2021-12-01 22:22:21 +0100

    finished Contact me🔥

--- src/PortfolioContainer/ContactMe/ContactMe.js
+++ src/PortfolioContainer/ContactMe/ContactMe.js
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
+import Typical from "react-typical";
 import ScreenHeading from "../../utilities/ScreenHeading/ScreenHeading";
 import ScrollService from "../../utilities/ScrollService";
 import Animations from "../../utilities/Animations";
+import imgBack from "../../../src/images/mailz.jpeg";
+import load1 from "../../../src/images/load2.gif";
 import "./ContactMe.css";
 
 export default function ContactMe(props) {
@@ -11,39 +14,66 @@ export default function ContactMe(props) {
     Animations.animations.fadeInScreen(props.id);
   };
 
-  let render = () => {
-    return <input type="text" value="" />;
-  };
-
   const fadeInSubscription =
     ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
 
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [banner, setBanner] = useState("");
+  const [bool, setBool] = useState(false);
+
+  const handleName = (e) => {
+    setName(e.target.value);
+  };
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
+  const handleMessage = (e) => {
+    setMessage(e.target.value);
+  };
+
   return (
-    <div className="contact-me-container screen-container" id={props.id || ""}>
-      <div className="contact-me-content">
-        <ScreenHeading title={"Contact Me"} subHeading={"Ask me anything"} />
-        <div className="contact-me-card">
-          <div className="contact-me-heading">
-            <span>Get In Touch📨</span>
-            <div className="contact-me-info"></div>
-            <div className="contact-me-box">
-              <div className="name-text-box">
-                <span>Name:</span>
-                <br />
-                {render()}
-              </div>
-              <div className="email-text-box">
-                <span>Email:</span>
-                <br />
-                {render()}
-              </div>
-              <div className="message-text-box">
-                <span>Message:</span>
-                <br />
-                {render()}
-              </div>
-            </div>
+    <div className="main-container" id={props.id || ""}>
+      <ScreenHeading subHeading={"Lets keep in tuch"} title={"Contact Me"} />
+      <div className="central-form">
+        <div className="col">
+          <h2 className="title">
+            <Typical loop={Infinity} steps={["Get In Thouch✉️", 1000]} />
+          </h2>
+          <a href="https://www.facebook.com/simonteodor.kobbenes/">
+            <i className="fa fa-facebook-square"></i>
+          </a>
+          <a href="https://www.instagram.com/simon_kobbenes/">
+            <i className="fa fa-instagram"></i>
+          </a>
+          <a href="https://www.linkedin.com/in/simon-kobbenes-8881961b5/">
+            <i className="fa fa-linkedin"></i>
+          </a>
+          <a href="https://github.com/H584958">
+            <i className="fa fa-github"></i>
+          </a>
+        </div>
+        <div className="back-form">
+          <div className="img-back">
+            <h4>Send your email here!</h4>
+            <img src={imgBack} alt="image not found" />
           </div>
+          <form>
+            <p>{banner}</p>
+            <label htmlFor="name">Name</label>
+            <input type="text" onChange={handleName} value={name} />
+            <label htmlFor="email">Email</label>
+            <input type="email" onChange={handleEmail} value={email} />
+            <label htmlFor="message">Message</label>
+            <textarea type="text" onChange={handleMessage} value={message} />
+            <div className="send-btn">
+              <button type="submit">
+                Send
+                <i className="fa fa-paper-plane" />
+              </button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
